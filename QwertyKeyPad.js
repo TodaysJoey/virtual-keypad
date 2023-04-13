ac03aef65475a9fed321a5625bd5a9c49a2d4b15
TodaysJoey <zihyun.yi@gmail.com>
2023-04-13 22:11:52 +0900

    키보드 내 한글 자판 유니코드 값 변경, 한영전환 기능 추가

--- QwertyKeyPad.js
+++ QwertyKeyPad.js
@@ -1,19 +1,58 @@
 const Keyboard = window.SimpleKeyboard.default;
+// const keyLayout = {
+//     layout : {
+//         default: [
+//           "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+//           "{tab} \u1107 \u110c \u1103 \u1100 \u1109 \u116d \u1167 \u1163 \u1162 \u1166 [ ] \\",
+//           "{lock} \u1106 \u1102 \u110b \u1105 \u1112 \u1169 \u1165 \u1161 \u1175 ; ' {enter}",
+//           "{shift} \u110f \u1110 \u110e \u1111 \u1172 \u116e \u1173 , . / {shift}",
+//           ".com @ {space}",
+//         ],
+//         shift: [
+//           "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+//           "{tab} \u1108 \u110d \u1104 \u1101 \u110a \u116d \u1167 \u1163 \u1164 \u1168 { } |",
+//           '{lock} \u1106 \u1102 \u110b \u1105 \u1112 \u1169 \u1165 \u1161 \u1175 : " {enter}',
+//           "{shift} \u110f \u1110 \u110e \u1111 \u1172 \u116e \u1173 < > ? {shift}",
+//           ".com @ {space}",
+//         ]
+//     },
+//     layoutCandidates: {},
+//     keyboardRef: null,
+//     stateToIgnore: null
+// };
+
+let buttonArray = [];
+let inputText = "";
 const keyLayout = {
     layout : {
         default: [
           "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
-          "{tab} \u1107 \u110c \u1103 \u1100 \u1109 \u116d \u1167 \u1163 \u1162 \u1166 [ ] \\",
-          "{lock} \u1106 \u1102 \u110b \u1105 \u1112 \u1169 \u1165 \u1161 \u1175 ; ' {enter}",
-          "{shift} \u110f \u1110 \u110e \u1111 \u1172 \u116e \u1173 , . / {shift}",
+          "{tab} \u3142 \u3148 \u3137 \u3131 \u3145 \u315B \u3155 \u3151 \u3150 \u3154 [ ] \\",
+          "{lock} \u3141 \u3134 \u3147 \u3139 \u314E \u3157 \u3153 \u314F \u3163 ; ' {enter}",
+          "{shift} \u314B \u314C \u314A \u314D \u3160 \u315C \u3161 , . / \uD55C/\uC601",
           ".com @ {space}",
         ],
         shift: [
           "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
-          "{tab} \u1108 \u110d \u1104 \u1101 \u110a \u116d \u1167 \u1163 \u1164 \u1168 { } |",
-          '{lock} \u1106 \u1102 \u110b \u1105 \u1112 \u1169 \u1165 \u1161 \u1175 : " {enter}',
-          "{shift} \u110f \u1110 \u110e \u1111 \u1172 \u116e \u1173 < > ? {shift}",
+          "{tab} \u3143 \u3149 \u3138 \u3132 \u3146 \u116d \u1167 \u1163 \u3152 \u3156 { } |",
+          '{lock} \u3141 \u3134 \u3147 \u3139 \u314E \u3157 \u3153 \u314F \u3163 : " {enter}',
+          "{shift} \u314B \u314C \u314A \u314D \u3160 \u315C \u3161 < > ? \uD55C/\uC601",
           ".com @ {space}",
+        ],
+
+        english: [
+              "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
+              "{tab} q w e r t y u i o p [ ] \\",
+              "{lock} a s d f g h j k l ; ' {enter}",
+              "{shift} z x c v b n m , . / \uD55C/\uC601",
+              ".com @ {space}",
+        ],
+        englishUpperCase: [
+              "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
+              "{tab} Q W E R T Y U I O P { } |",
+              '{lock} A S D F G H J K L : " {enter}',
+              "{shift} Z X C V B N M < > ? \uD55C/\uC601",
+              ".com @ {space}",
         ]
     },
     layoutCandidates: {}
@@ -24,98 +63,58 @@ const myKeyboard = new Keyboard({
     onKeyPress: button => onKeyPress(button),
     ...keyLayout
 });
-
-/**
- * Update simple-keyboard when input is changed directly
- */
-// document.querySelector(".input").addEventListener("input", event => {
-//     myKeyboard.setInput(event.target.value);
-// });
   
 function onChange(input) {
-    document.querySelector(".input").value = input;
+    document.querySelector(".input").value = inputText;
     console.log("Input changed", input);
-    
-    var temp = 'ㅇ';
-    console.log('test:' + temp.charCodeAt());
-    let pre = Hangul.disassemble(input);
-    console.log(pre);
-    for(var i=0; i<pre.length; i++) {
-        console.log(pre[i]);
-        console.log(pre[i].charCodeAt());
-
-        seperate(pre[i]);
-    }
-    console.log('disassemble the end');
-
-    // 아스키코드 기준으로 assemble
-    let curr = Hangul.assemble(pre);
-    console.log(curr);
 }
   
 function onKeyPress(button) {
     console.log("Button pressed", button);
-    /**
-     * * If you want to handle the shift and caps lock buttons
-     * */
-    if (button === "{shift}" || button === "{lock}") handleShift();
+    if (!["{shift}",
+          "{language}",
+          "{enter}",
+          "{bksp}",
+          "{space}",
+          "{tab}",
+          "한/영"
+        ].includes(button)
+      ) { buttonArray.push(button); }
+      
+      if (button === "{bksp}")  buttonArray.pop();
+      if (button === "{space}") buttonArray.push(" ");
+      if (button === "{tab}")   buttonArray.push("  ");
+      }
+      
+      
+    inputText = Hangul.assemble(buttonArray);
+      
+    if (button === "{shift}" || button === "{lock}") handleShift(); // shift 키 잠금 기능
+    if (button === "한/영") handleChangeLanguage();                 // 한,영 전환 기능
 }
 
 function handleShift() {
     let currentLayout = myKeyboard.options.layoutName;
-    console.log(currentLayout);
-    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+    let shiftToggle = "";
+    if(currentLayout === "default") {
+        shiftToggle = "shift";
+    } else if(currentLayout === "english") {
+        shiftToggle = "englishUpperCase";
+    } else if(currentLayout === "shift") {
+        shiftToggle === "default";
+    } else {
+        shiftToggle === "english";
+    }
   
     myKeyboard.setOptions({
       layoutName: shiftToggle
     });
-  }
+}
 
-
-function seperate(str) {
-    debugger;
-    let sb = [];
-    // 초성 19개
-    var arr_cho =
-            ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
-
-    // 중성 21개
-    var arr_jung =
-            ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"];
-
-    // 종성 28개
-    var arr_jong =
-            ["", "ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
-
-    // StringBuffer sb = new StringBuffer();
-    for (let i=0; i<str.length; i++) {
-        let uniVal = str.charCodeAt(); // 자바스크립트의 charCodeAt은 정수 반환
-        uniVal = '0x'+uniVal.toString(16);
-        // console.log(uniVal);
-
-        
-        try {
-            debugger;
-            // if (uniVal >= 0xAC00 && uniVal <= 0xD7A3) {
-            if (parseInt(uniVal,16) >= parseInt('0x1100',16) && parseInt(uniVal,16) <= parseInt('0x1112', 16)) {
-                uniVal = String.fromCharCode(parseInt(uniVal,16)  + parseInt('0x2031',16));
-                // let cho = String.fromCharCode(uniVal / 28 / 21);
-                // let jung = String.fromCharCode(uniVal / 28 % 21);
-                // let jong = String.fromCharCode(uniVal % 28);
-                debugger;
-                sb.push(String.fromCharCode(uniVal));
-                // sb.push(jung);
-                // sb.push(jong);
-            } else {
-                sb.push(String.fromCharCode(uniVal));
-            }
-        } catch {
-            sb.push(String.fromCharCode(uniVal));
-        }
-    }
-
-    console.log('result:');
-    console.log(sb);
-
-    return sb.toString();
+function handleChangeLanguage() {
+    let currentLayout = myKeyboard.options.layoutName;
+    let langToggle = currentLayout === "default" ? "english" : "default";
+    myKeyboard.setOptions({
+        layoutName: langToggle
+    });
 }
